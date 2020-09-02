680d5616c7df7b6f7ac533d7364d02faaa090ef7
Dominik Klimpera <dominikklimpera@seznam.cz>
2020-09-02 21:57:48 +0200

    first js

---
+++ doma.js
@@ -0,0 +1,13 @@
+// alert("hello");
+// console.log("wau");
+
+var b = "smothie";
+console.log(b)
+
+var somenumber = 45;
+console.log(somenumber);
+
+// document.getElementById("sometext").innerHTML = "Hey there";
+
+var age = prompt("what is your age?");
+document.getElementById("sometext").innerHTML = age;
