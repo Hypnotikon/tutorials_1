846d41367b581fc224b6414f6b3796f30191b470
Dominik Klimpera <dominikklimpera@seznam.cz>
2020-09-02 22:56:27 +0200

    1

--- doma.js
+++ doma.js
@@ -1,13 +1,82 @@
 // alert("hello");
 // console.log("wau");
 
-var b = "smothie";
-console.log(b)
+// var b = "smothie";
+// console.log(b)
 
-var somenumber = 45;
-console.log(somenumber);
+// var somenumber = 45;
+// console.log(somenumber);
 
 // document.getElementById("sometext").innerHTML = "Hey there";
 
-var age = prompt("what is your age?");
-document.getElementById("sometext").innerHTML = age;
+// var age = prompt("what is your age?");
+// document.getElementById("sometext").innerHTML = age;
+
+// var num1 = 10;
+// num1 = num1 + 1
+// num1++
+// num1--
+// console.log(num1 / 6);
+// num1 +=10;
+// console.log(num1);
+
+/* Functions
+ */
+// function fun() {
+// alert("this is a function");
+// }
+//
+// fun();
+
+
+// function greeting(){
+//   var jmeno = prompt("jake je tvoje jmeno? ");
+//   result = console.log("tvoje jméno je" + " " + jmeno);
+// }
+//
+// greeting();
+
+// function sumNumbers(num1, num2){
+//   var result = num1 + num2;
+//   console.log(num1+ num2);
+// }
+//
+// sumNumbers(10, 10);
+
+
+// num = 0
+// while (num < 100) {
+//   num += 1;
+//   console.log(num);
+// }
+
+
+// for (let num = 0; num<=100; num++) {
+//   console.log(num);
+// }
+
+/* data types
+let yourAge = 18;   // number
+let yourName = "bob";   //string
+let name = {first: "jane", last: "Doe"}; //object
+let truth = false; //boolean
+let groceris = ["apple", "banana", "oranges"]; //
+let random; //undefined
+let nothing = null; //value null
+*/
+
+
+let fruit = "banana, apple, orange, blackberry";
+let morefruits = "banana\napple";
+console.log(fruit.type);
+console.log(fruit.indexOf("b"));
+console.log(fruit.replace("ban", "123"));
+console.log(fruit.toUpperCase());
+console.log(fruit.toLowerCase());
+console.log(fruit[2]);
+console.log(fruit.split(","));
+console.log(fruit.split(""));
+
+
+// let x = "meda"
+// console.log(x.indexOf("m"));
